45d96a1840313c66c88a3ed44b7d502e0eb3f045
DeepPatel2211 <deepxpatel33@gmail.com>
2025-01-29 17:01:24 -0600

    feat: added badge component

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -5,7 +5,6 @@ import { Pressable } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/native";
 
-import TestingScreen from "@/components/Testing";
 import NotFoundScreen from "@/app/NotFound";
 import HomeScreen from "@/app/Feeds/";
 import LoginScreen from "@/app/Login/";
@@ -17,10 +16,10 @@ import SignupScreen from "@/app/Signup/";
 import TermsScreen from "@/app/Signup/Terms";
 import PrivacyPolicyScreen from "@/app/Signup/PrivacyPolicyScreen";
 
-<<<<<<< Updated upstream
+
 import pb from "@root/pocketbase.config";
 import { useState, useEffect } from "react";
-=======
+
 function DrawerNavigation() {
   const Drawer = createDrawerNavigator();
   
@@ -30,7 +29,7 @@ function DrawerNavigation() {
       drawerInactiveTintColor: Colors.night,
       drawerActiveTintColor: Colors.light.blue
      }}>
-      <Drawer.Screen name="Home" component={TestingScreen} options={{ drawerItemStyle: { display: "none" }}} />
+      <Drawer.Screen name="Home" component={HomeScreen} options={{ drawerItemStyle: { display: "none" }}} />
       <Drawer.Screen name="Profile" component={ProfileScreen} options={{
         drawerLabelStyle: {
           fontSize: 20,
@@ -70,7 +69,7 @@ function DrawerNavigation() {
     </Drawer.Navigator>
   );
 }
->>>>>>> Stashed changes
+
 
 export default function Navigation() {
   const Stack = createNativeStackNavigator();
